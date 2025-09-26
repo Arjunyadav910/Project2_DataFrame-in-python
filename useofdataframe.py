db897d2773ede31f370e27fea31b1d93da51a4a7
Arjunyadav910 <arjunyadav667733@gmail.com>
2025-09-26 14:00:45 +0530

    Initial commit

---
+++ useofdataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+Data={
+    "Name":["Arjun","Anurag","Archana","Priyanshu","Akash"],
+    "Address":["Jaunpur","Agra","Mujaffarpur","Patna","Jaunpur"],
+    "Phone":[2682656234,5454463254,6863526432,5686286356,5487548798]
+}
+Df=pd.DataFrame(Data)
+print(Df)
+# Df it is use to print without calling print function and data output is visually good
